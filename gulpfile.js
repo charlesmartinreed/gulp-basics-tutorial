b4867d8f2a0fac806f193cd8b289cd61f8b2fec1
charlesmartinreed <charlesmartinreed@icloud.com>
2019-05-07 14:17:54 -0500

    Demonstrate basics of minifying images using gulp-image.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require("gulp");
+const imagemin = require("gulp-image");
 const { series } = require("gulp");
 
 // private task = not exported, used internally in series() or parallel() composition
@@ -14,7 +15,7 @@ const { series } = require("gulp");
 /* six ways to signal async completion in Gulp 4+: return a stream, return a promise, return a child process, callback, return an event emitter, return an observable. */
 
 function message(cb) {
-  console.log("HTML files successfully copied");
+  console.log("HTML files successfully copied, images minified");
   // callback to signal async complete
   cb();
 }
@@ -25,5 +26,11 @@ function copyHTML(cb) {
   cb();
 }
 
+// optimize images
+function imageMin(cb) {
+	gulp.src('src/images/*').pipe(imagemin()).pipe(gulp.dest('dist/images'))
+	cb();
+}
+
 exports.message = message;
-exports.default = series(copyHTML, message);
+exports.default = series(imageMin, copyHTML, message);
